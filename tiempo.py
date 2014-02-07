acc2473185fc6b94790d4f9270d9ba763f3c5313
carlosfdez91 <carlosfernandez16@gmail.com>
2014-02-07 09:57:00 +0100

    Añadida la lista de cuidades

--- tiempo.py
+++ tiempo.py
@@ -1,18 +1,20 @@
-#-*- coding: utf-8-*-
+#coding: utf-8
 import requests
 import json
 
-ciudad = raw_input("""1. Almeria
-2. Cadiz
-3. Cordoba
+lista = {"1":"Almería","2":"Cádiz","3":"Córdoba","4":"Granada","5":"Huelva","6":"Jaén","7":"Málaga","8":"Sevilla"}
+
+ciudades = raw_input("""1. Almería
+2. Cádiz
+3. Córdoba
 4. Granada
 5. Huelva
-6. Jaen
-7. Malaga
+6. Jaén
+7. Málaga
 8. Sevilla
 
-¿De que ciudad quieres saber la temperatura actual? """)
+¿De qué ciudad quieres saber la temperatura actual? """)
 
-lista = {"1":"Almeria","2":"Cadiz","3":"Cordoba","4":"Granada","5":"Huelva","6":"Jaen","7":"Malaga","8":"Sevilla"}
+print lista[ciudades]
 
 #respuesta = requests.get('http://api.openweathermap.org/data/2.5/weather',params={'q':'Malaga,spain'})
