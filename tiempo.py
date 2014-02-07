cbe7868d3c71da726562e57c941e0e7a5d926dc4
carlosfdez91 <carlosfernandez16@gmail.com>
2014-02-07 10:32:16 +0100

    Aplicación casi finalizada

--- tiempo.py
+++ tiempo.py
@@ -2,7 +2,7 @@
 import requests
 import json
 
-lista = {"1":"Almería","2":"Cádiz","3":"Córdoba","4":"Granada","5":"Huelva","6":"Jaén","7":"Málaga","8":"Sevilla"}
+lista = {"1":"Almeria","2":"Cadiz","3":"Cordoba","4":"Granada","5":"Huelva","6":"Jaen","7":"Malaga","8":"Sevilla"}
 
 ciudades = raw_input("""1. Almería
 2. Cádiz
@@ -15,6 +15,13 @@ ciudades = raw_input("""1. Almería
 
 ¿De qué ciudad quieres saber la temperatura actual? """)
 
-print lista[ciudades]
 
-#respuesta = requests.get('http://api.openweathermap.org/data/2.5/weather',params={'q':'Malaga,spain'})
+respuesta = requests.get('http://api.openweathermap.org/data/2.5/weather',params={'q':'%s,spain' % lista[ciudades]})
+
+dicc = json.loads(respuesta.text)
+
+tempkelvin = dicc["main"]["temp"]
+
+tempreal = tempkelvin - 273
+
+print "La temparatura actual de %s es %s grados centígrados" % (lista[ciudades],tempreal)
